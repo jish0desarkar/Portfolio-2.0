4e178fd8989721a7fa3f05411832e99a3caa3b57
Jishnu De Sarkar <desarkarjishnu@gmail.com>
2019-03-03 04:27:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.getElementById("nav").classList.add("nav-bar-sm");
+    document.getElementById("dropdown").classList.remove("nav-bar-sm");
+  }
+  else{
+    document.getElementById("nav").classList.remove("nav-bar-sm");
+    document.getElementById("dropdown").classList.add("nav-bar-sm");
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
